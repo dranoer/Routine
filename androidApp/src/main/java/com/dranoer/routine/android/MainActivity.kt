44090f175ab2e542c88d2bd103db62249f9de9a1
nazaninnasab <dranoer@gmail.com>
2023-08-20 22:29:48 +0200

    Add flowrow

--- androidApp/src/main/java/com/dranoer/routine/android/MainActivity.kt
+++ androidApp/src/main/java/com/dranoer/routine/android/MainActivity.kt
@@ -3,10 +3,8 @@ package com.dranoer.routine.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
@@ -14,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.dranoer.routine.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,33 +22,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    FlowView()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterialApi::class)
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
-fun GreetingView(text: String) {
-
+fun FlowView() {
     FlowRow(modifier = Modifier.padding(8.dp)) {
-        List(11) {
-            Chip(modifier = Modifier.padding(4.dp), onClick = {}) {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Text(text = "Item")
-                    Text(text = "Another item")
-                }
-            }
-        }
+        TargetChip(text = "Column Item")
+        TargetChip(text = "Another item")
+        TargetChip(text = "Third item")
+        TargetChip(text = "Forth item")
+        TargetChip(text = "Fifth item")
+        TargetChip(text = "Sixth item")
+        TargetChip(text = "Seventh item")
+        TargetChip(text = "Eight item")
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun TargetChip(text: String) {
+    Chip(modifier = Modifier.padding(4.dp), onClick = {}) { Text(text = text) }
+}
+
+//region Preview
+@Preview(showBackground = true)
+@Composable
+private fun FlowViewPreview() {
+    MyApplicationTheme {
+        FlowView()
     }
 }
 
 @Preview
 @Composable
-fun DefaultPreview() {
+private fun TargetChipPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        TargetChip("Target")
     }
 }
+//endregion
